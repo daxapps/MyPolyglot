2ee54eb8cc0d6fcd018523d893079879aa7adf43
daxapps <daxapps777@gmail.com>
2017-03-09 08:07:54 -0600

    Build a Today Extension - Add Today extension file - Add tableView to UI

---
+++ Widget/TodayViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TodayViewController.swift
+//  Widget
+//
+//  Created by Jason Crawford on 3/9/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet weak var tableView: UITableView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
