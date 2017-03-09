ad6a3188d12afa67365bfe4d6070b95d261313ea
daxapps <daxapps777@gmail.com>
2017-03-09 08:34:02 -0600

    Enable Extension to Work - Add entitlements via Capabilities-> App Groups(add group string)

--- Widget/TodayViewController.swift
+++ Widget/TodayViewController.swift
@@ -9,13 +9,64 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-        
+    
+    var words = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        
+        if let defaults = UserDefaults(suiteName: "group.com.DaxApps.MyPolyglot") {
+            if let savedWords = defaults.object(forKey: "Words") as? [String] {
+                
+                words = savedWords
+            }
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        let word = words[indexPath.row]
+        let split = word.components(separatedBy: "::")
+        
+        cell.textLabel?.text = split[0]
+        
+        cell.detailTextLabel?.text = ""
+        
+        cell.selectedBackgroundView = UIView()
+        cell.selectedBackgroundView!.backgroundColor = UIColor(white: 1, alpha: 0.20)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            
+            if cell.detailTextLabel?.text == "" {
+                let word = words[indexPath.row]
+                let split = word.components(separatedBy: "::")
+                
+                cell.detailTextLabel?.text = split[1]
+                
+            } else {
+                cell.detailTextLabel?.text = ""
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
