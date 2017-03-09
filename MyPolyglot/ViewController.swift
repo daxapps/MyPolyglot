64e9877c6c85705935dc26383e10494a4ab36151
daxapps <daxapps777@gmail.com>
2017-03-09 07:57:01 -0600

    Enable Add and Remove Words

--- MyPolyglot/ViewController.swift
+++ MyPolyglot/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewWord))
+        
         let titleAttributes = [NSFontAttributeName: UIFont(name: "AmericanTypewriter", size: 22)!]
         navigationController?.navigationBar.titleTextAttributes = titleAttributes
         title = "POLYGLOT"
@@ -82,6 +84,67 @@ class ViewController: UITableViewController {
                 cell.detailTextLabel?.text = ""
             }
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        words.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        saveWords()
+    }
+    
+    func saveWords() {
+        
+        if let defaults = UserDefaults(suiteName: "group.com.DaxApps.MyPolyglot") {
+            defaults.set(words, forKey: "Words")
+        }
+    }
+    
+    func addNewWord() {
+        
+        //create our alert controller
+        let ac = UIAlertController(title: "Add new word", message: nil, preferredStyle: .alert)
+        
+        //add two text fields, one for english and one for french
+        ac.addTextField { (textField) in
+            textField.placeholder = "English"
+        }
+        
+        ac.addTextField { (textField) in
+            textField.placeholder = "French"
+        }
+        
+        //create an Add Word button that submits the users input
+        let submitAction = UIAlertAction(title: "Add Word", style: .default) { [unowned self, ac]
+            (action: UIAlertAction!) in
+            
+            //pull out the english and french words, or an empty string if there was a problem
+            let firstWord = ac.textFields?[0].text ?? ""
+            let secondWord = ac.textFields?[1].text ?? ""
+            
+            //submit the english and french word to the inserFlashcard() method
+            self.insertFlashcard(first: firstWord, second: secondWord)
+        }
+        
+        //add the submit action, plus a cancel button
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        //present the alert controller to the user
+        present(ac, animated: true)
+        
+    }
+    
+    func insertFlashcard(first: String, second: String) {
+        
+        guard first.characters.count > 0 && second.characters.count > 0 else { return }
+        
+        let newIndexPath = IndexPath(row: words.count, section: 0)
+        
+        words.append("\(first)::\(second)")
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        
+        saveWords()
     }
     
 
